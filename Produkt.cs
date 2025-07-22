17bb1bd2d71c17629329a8ba5830160a1163938a
FridaBergkvist2 <fridaber10@student.astar.se>
2025-07-22 11:40:07 +0200

    Försökt rensa koden lite, börjar bli så förvirrad av allt klippande och klistrande

---
+++ Produkt.cs
@@ -0,0 +1,16 @@
+﻿public class Produkt
+{
+    public string Namn { get; set; }
+    public double Pris { get; set; }
+
+    public Produkt(string namn, double pris)
+    {
+        Namn = namn;
+        Pris = pris;
+    }
+
+    public override string ToString()
+    {
+        return $"{Namn} - {Pris:C}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Storage<Produkt> produktStorage = new Storage<Produkt>();
+	{
+	Läsinfrånfil(produktStorage)
+	MataInProdukter(produktStorage);
+
+	console.WriteLine("\n---Böcker jag lagt till---");
+
+
+	SkrivUtAlla(produktStorage)
+
+        Console.WriteLine("Programmet avslutas.");
+    }
+
+    static void MataInProdukter(Storage<Produkt> storage)
+    {
+        while (true)
+
+        {
+
+            try
+            {
+                Console.Write("Ange produktnamn (eller 'stopp'): ");
+                string namn = Console.ReadLine();
+                if (namn.ToLower() == "stopp")
+                    break;
+
+                Console.Write("Ange pris: ");
+                double pris = Convert.ToDouble(Console.ReadLine());
+
+		var produkt = new Produkt (namn, pris);
+		storage.LäggTill(new Produkt(namn, pris));
+		
+		string rad = $"{produkt.Namn};{produkt.Pris}";
+ 		File.AppendAllText("testfil.txt", rad + Environment.NewLine);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Fel: Ange ett korrekt pris (t.ex. 199,50).");
+            }
+    static void LäsInFrånFil(Storage<Produkt> storage)
+    	{
+	if (File.Exists(textfil.txt)
+
+        {
+
+            try
+            {
+                Console.Write("Ange produktnamn (eller 'stopp'): ");
+                string namn = Console.ReadLine();
+                if (namn.ToLower() == "stopp")
+                    break;
+
+                Console.Write("Ange pris: ");
+                double pris = Convert.ToDouble(Console.ReadLine());
+
+		var produkt = new Produkt (namn, pris);
+		storage.LäggTill(new Produkt(namn, pris));
+		
+		string rad = $"{produkt.Namn};{produkt.Pris}";
+ 		File.AppendAllText("testfil.txt", rad + Environment.NewLine);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Fel: Ange ett korrekt pris (t.ex. 199,50).");
+            }
+        }
+        }
+    }
+}
